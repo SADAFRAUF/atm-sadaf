920ea95f463b02daaa8ce83e73a2056f2a144572
SADAF RAUF QURESHI <162603274+SADAFRAUF@users.noreply.github.com>
2024-04-22 00:07:24 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 15000;//Dollar
+
+
+
+let myPin = 8888;
+
+
+let pinAnswer = await inquirer.prompt([
+    {
+        name:"Sadaf",
+        message:"enter your pin",
+        type:"number", 
+}]
+);
+
+if(pinAnswer.Sadaf === myPin)
+    console.log("Correct pin code!!!")
+let operationAns = await inquirer.prompt([{
+    name:"operation",
+    message:"please select option",
+    type:"list",
+    choices:["withdraw","check balance"]
+
+}
+]
+);
+
+console.log(operationAns);
+
+if (operationAns.operation === "withdraw") {
+    let amountAns =await inquirer.prompt([
+        {
+            name: "amount",
+            message: "enter your amount",
+            type: "number",
+    }
+
+]);
+    //=,-=,+=
+    myBalance -= amountAns.amount;
+
+    console.log("Your remaining balance is: " + myBalance)
+
+}else if(operationAns.operation === "check balance"){
+    console.log("your balance is: " + myBalance)
+}
+
+else {
+    console.log("Incorrect pin number");
+}
